81bdb251f9adbd2a81728f97fd221bd58d721811
Ushpik <Ushpikad61@gmail.com>
2024-03-21 16:16:54 +0600

    modified:   main/main.py

--- main/main.py
+++ main/main.py
@@ -1,3 +1,12 @@
-print ('Hello, World')
-print ("Пожалуй, это первое изменение")
-print ("Пожалуй, это второе изменение")
+# Составление GET-запроса и получение ответа
+
+import requests
+
+api_url = 'http://numbersapi.com/40'
+
+response = requests.get(api_url)  # Отправляем GET-запрос и сохраняем ответ в переменной response
+
+if response.status_code == 200:  # Если код ответа на запрос - 200, то смотрим, что пришло в ответе
+    print(response.text)
+else:
+    print(response.status_code)  # При другом коде ответа выводим этот код
